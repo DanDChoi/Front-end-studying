302bc90be434f14ac91d2c49129914fd8ff44afa
DanDChoi <showbegin622@gmail.com>
2021-08-19 18:10:07 +0900

    8/19

--- app.js
+++ app.js
@@ -26,3 +26,62 @@ console.log(daysOfWeek);
 daysOfWeek.push("sun");
 
 console.log(daysOfWeek);
+
+const player = {
+    name: "DH",
+    points: 10,
+    fat: true,
+};
+console.log(player);
+player.points = player.points + 15;
+console.log(player);
+
+function sayHello(nameOfPerson, age){
+    console.log("Hello my name is " + nameOfPerson + "and I am " + age +" years old");
+}
+
+sayHello("nico", 10);
+sayHello("dal", 23);
+sayHello("lynn", 21);
+
+
+function plus(a, b){
+    console.log(a + b)
+}
+function divide(a, b){
+    console.log(a / b);
+}
+plus(1, 3)
+plus(12, 3)
+divide(50, 10)
+
+const player = {
+    name: "Otani",
+    sayHello: function (otherPesronsName) {
+        console.log("Hello!" + otherPesronsName + " nice to meet you");
+    },
+};
+
+console.log(player.name);
+player.sayHello("tlynn");
+
+
+const calculator = {
+    add: function (a , b) {
+    console.log(a + b);
+    },
+    minus: function (a , b) {
+    console.log(a - b);
+    },
+    divide: function (a , b) {
+    console.log(a / b);
+    },
+    powerOf: function (a , b) {
+    console.log(a ** b);
+    },
+};
+
+calculator.add(2 , 2);
+calculator.minus(2 , 2);
+calculator.divide(2 , 2);
+calculator.powerOf(2 , 2);
