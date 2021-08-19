9d1c7da60439bb31fc8354ed5c602ef30b791969
Dan Daehyun Choi <showbegin622@gmail.com>
2021-08-19 13:11:52 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const a = 5;
 const b = 2;
-
-const myName = "Dan";
+let myName = "Dan";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("Hello " + myName);
+
+myName = "Daehyun"
+
+console.log("Your new name is " + myName);
+
+
+const amIFat = true;
+console.log(amIFat);
+
+
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// Get Item from Array
+console.log(daysOfWeek);
+
+//Add one more day to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
