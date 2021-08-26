3985bcf2bd420763064ef585fc94a0daa66996f2
Dan Daehyun Choi <showbegin622@gmail.com>
2021-08-27 01:47:40 +0900

    8/26 last

--- js/quotes.js
+++ js/quotes.js
@@ -44,4 +44,4 @@ const quotes = [
 const quote = document.querySelector("#quote span:first-child");
 const author = document.querySelector("#quote span:last-child");
 
-console.log(quotes[Math.floor(Math.random() * 10)]);
+console.log(quotes[Math.floor(Math.random() * quotes.lengths)]);
