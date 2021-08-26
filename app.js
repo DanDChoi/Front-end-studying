6b42ce421a1a16acf7bfd35adf28c99276d6bdd8
DanDChoi <showbegin622@gmail.com>
2021-08-26 16:08:46 +0900

    8/26-2-1

--- app.js
+++ app.js
@@ -2,28 +2,28 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
-const HIDDEN_ClASSNAME = "hidden";
+const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
  //`Hello ${username}' 과 "Hello " + username과 같음//
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    loginForm.classList.add("hidden");
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem("USERNAME_KEY", username);
-    paintGreetings(savedUsername);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
 function paintGreetings(username) {
     greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_ClASSNAME);
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-const savedUsername = localStorage.getItem("USERNAME_KEY");
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if(savedUsername === null) {
-    loginForm.classList.remove(HIDDEN_ClASSNAME);
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
     paintGreetings(savedUsername);
