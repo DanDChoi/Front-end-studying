33d762f1a381bf44e75ae5390b591600af8b8ee9
DanDChoi <showbegin622@gmail.com>
2021-08-26 15:58:45 +0900

    8/26-2

--- app.js
+++ app.js
@@ -3,13 +3,28 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_ClASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+ //`Hello ${username}' 과 "Hello " + username과 같음//
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add("hidden");
     const username = loginInput.value;
-    greeting.innerText = `Hello ${username}`; //"Hello " + username과 같음//
+    localStorage.setItem("USERNAME_KEY", username);
+    paintGreetings(savedUsername);
+}
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_ClASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem("USERNAME_KEY");
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_ClASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
