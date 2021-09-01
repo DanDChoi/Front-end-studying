f12cc9d0da1d69d9ac7f1a04be3ee48c5cc8b8e2
DanDChoi <showbegin622@gmail.com>
2021-09-01 17:01:34 +0900

    9/1-1

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,6 @@
 const API_KEY = "e5d64c908d1991956c3a77aa1451cc97";
 
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
@@ -7,10 +8,10 @@ function onGeoOk(position) {
     fetch(url)
     .then(response => response.json())
     .then(data => {
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
+        const weather = document.querySelector("#weather div:first-child");
+        const city = document.querySelector("#weather div:last-child");
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
         city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
 }
 
